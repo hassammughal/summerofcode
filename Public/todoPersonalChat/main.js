23a3169a7adff9181abd3ac1fdaa782330a374db
Hassam Mughal <Hassam Mughal>
2016-07-27 19:17:46 +0500

    heroku

--- Public/todoPersonalChat/main.js
+++ Public/todoPersonalChat/main.js
@@ -1,60 +1,10 @@
 var DATA = [];
 load();
 
- $("#submit").click(function(value){
-    var takeInput = $('#input').val();
-    
-    DATA.push(takeInput);
-    RenderJSON(DATA);
-    save();
-    $("#input").val('');
-});
 
-function RenderJSON(data){
-
-        $('#container').empty();
-        for(var i=0; i<data.length;i++){
-            
-        $('#container').append("<div id="+i+"><input type='checkbox'/> <label> "+data[i]+"</label><br></div>");
-            
-        $('#'+i).click(function(){
-            var index = $(this).attr("id");
-            $(this).remove();
-
-            //var index = DATA.indexOf(myId);
-            if(index > -1){
-                DATA.splice(index,1);
-                save();
-            }
-        });
-    }
-}
-
-function save(){
-    // localStorage.myData = JSON.stringify(DATA);
-    $.ajax({
-	    url: '/setTodo', 
-	    type: 'POST', 
-	    contentType: 'application/json', 
-	    data: JSON.stringify(DATA),
-	    success:function(res){console.log(res);}
-	});
-}
-
-function load(){
-
-    // DATA = JSON.parse(localStorage.myData);
-    // RenderJSON(DATA);
-    $.get('/getTodo',function(res){
-		DATA = res;
-        RenderJSON(DATA);
-
-        console.log(res);   
-	});
-}        
-
-
-$("#fsubmit").click(function(value){
+// The method fires when user 1 presses send button
+// The method pushes data to the DATA[] array, Renders and saves
+ $("#fsubmit").click(function(value){
     var takeInput = $('#finput').val();
     
     DATA.push("sumair:"+takeInput);
@@ -97,7 +47,7 @@ function RenderJSON(data){
 // This method sends data to the server and stores it there.
 function save(){
     $.ajax({
-	    url: '/setTodo', 
+	    url: 'http://datastore.asadmemon.com/sumair', 
 	    type: 'POST', 
 	    contentType: 'application/json', 
 	    data: JSON.stringify(DATA),
@@ -108,9 +58,10 @@ function save(){
 // This method loads data from the server
 function load(){
 
-    $.get('/goTodo',function(res){
+    $.get('http://datastore.asadmemon.com/sumair',function(res){
 		DATA = res;
         RenderJSON(DATA);
+
     
 	});
 }  
